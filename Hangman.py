4bb260cea3d91d870ba1e41c5fe1d06a77011f0d
saad_musema <Saadmusema3@gmail.com>
2022-06-12 10:31:35 +0300

    Updated the code

--- Hangman.py
+++ Hangman.py
@@ -17,23 +17,26 @@ word_length = len(chosen_word)
 # print(display)
 # guess = ""
 end_of_game = False
-lives = 6
+lives = len(stages) - 1
 used_char = []
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    used_char += guess 
+    # used_char += guess
 
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
     print(f"{' '.join(display)}")
-    lives -= 1
-    print(stages[lives])
+
+    if guess not in chosen_word:
+        print(f"You have guessed wrong, you have{lives} remaining")
+        lives -= 1
     if lives == 0:
         end_of_game = True
         print("You Lose!")
     if "_" not in display:
         end_of_game = True
         print("You Win!")
+    print(stages[lives])
 print(f"The word was{chosen_word}")
